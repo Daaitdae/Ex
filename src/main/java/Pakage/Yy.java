3157ac6214f096e5b4f35eac16cdbc4fff25bf4e
Administrator <Administrator@ITK-20210320EAU>
2023-08-16 11:59:47 +0900

    Yy test

--- src/main/java/Pakage/Yy.java
+++ src/main/java/Pakage/Yy.java
@@ -1,18 +1,18 @@
 package Pakage;
 
-public class ex {
+public class Yy {
 
 	public int weight;
 	public String name;
 	
 	
-	public ex() {
+	public Yy() {
 		weight=55;
 		name="미미";
 	}
 	
 
-	public ex(int w,String n) {
+	public Yy(int w,String n) {
 		System.out.println("몸무게는"+w);
 		System.out.println("이름"+n);
 		weight=w;
